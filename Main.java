0b3a24dcba3c1f063e9f9b9e1d043fb4396f34c3
chrisbenoi <162717533+chrisbenoi@users.noreply.github.com>
2026-03-02 17:31:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -44,27 +44,40 @@ class ThreeHourSubject extends Subject {
 
 class Student {
     Subject[] subjects;
+    double CurrentGpa;
+    int CurrentHours;
 
-    Student(Subject[] subjects) {
+    Student(Subject[] subjects, double currentGpa, int currentHours) {
         this.subjects = subjects;
+        this.CurrentGpa = currentGpa;
+        this.CurrentHours = currentHours;
     }
-
     double calculateGPA() {
         double totalPoints = 0;
         int totalCredits = 0;
+
         for (Subject s : subjects) {
             totalPoints += s.getGradePoints();
             totalCredits += s.credits;
         }
-        return totalPoints / totalCredits;
+        if (CurrentHours == 0)
+            return totalPoints / totalCredits;
+        else {
+            return (totalPoints + (CurrentGpa * CurrentHours)) / (CurrentHours + totalCredits);
+        }
     }
 }
 
 public class Main {
-    static void main(String[] args) {
+    static void main() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("===== GPA Calculator (4.0 scale) =====");
+        System.out.println("Enter your current GPA: ");
+        double currentGpa = input.nextDouble();
+        System.out.println("How many hours have you completed in college ? ");
+        int currentHours = input.nextInt();
+
 
         int n;
         while (true) {
@@ -107,7 +120,7 @@ public class Main {
                 } else {
                     System.out.println("Invalid input! Enter a number.");
                     input.next();
-                }
+                }3.19
             }
 
             if (hours == 2)
@@ -116,9 +129,8 @@ public class Main {
                 subjects[i] = new ThreeHourSubject(grade);
         }
 
-        Student s1 = new Student(subjects);
+        Student s1 = new Student(subjects, currentGpa, currentHours);
         System.out.printf("\nStudent GPA: %.2f\n", s1.calculateGPA());
-
         input.close();
     }
 }
